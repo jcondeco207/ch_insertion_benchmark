76deb2f0ef392e99eafb9bf8ced73f0e99d9ad9c
jcondeco207 <joao.r.condeco@gmail.com>
2025-07-04 13:25:41 +0100

    Implement ClickhouseConnector class with session management and database operations stubs

---
+++ py_benchmarks/clickhouse_connect_bm.py
@@ -0,0 +1,21 @@
+from clickhouse_connect import create_client
+
+
+class ClickhouseConnector:
+    def __init__():
+        pass
+
+    def open_session():
+        pass
+
+    def close_session():
+        pass
+
+    def list_databases():
+        pass
+
+    def create_database():
+        pass
+
+    def delete_database():
+        pass
