9519493ccc2bbd3f04bd31e1c94d6c94d8d8eb2b
jcondeco207 <joao.r.condeco@gmail.com>
2025-07-05 02:49:13 +0100

    Refactor ClickhouseConnector class to implement session management and database operations

--- py_benchmarks/clickhouse_connect_bm.py
+++ py_benchmarks/clickhouse_connect_bm.py
@@ -1,21 +1,43 @@
-from clickhouse_connect import create_client
+import clickhouse_connect
+import os, traceback
+from dotenv import load_dotenv
 
 
 class ClickhouseConnector:
-    def __init__():
-        pass
+    def __init__(self):
+        self.open_session()
+        print(f"[ CH CONNECTION ] : {self.client.ping()}")
 
-    def open_session():
-        pass
+    def open_session(self):
+        try:
+            load_dotenv(dotenv_path='.env_local')
+            self.client = clickhouse_connect.get_client(
+                host=os.getenv('CLICKHOUSE_HOST', 'localhost'),
+                username=os.getenv('CLICKHOUSE_USER', 'demo'),
+                password=os.getenv('CLICKHOUSE_PASSWORD', 'demo')
+            )
+        except Exception as e:
+            print(f"[ ERROR ]: Failed to open session.")
 
-    def close_session():
-        pass
+    def close_session(self):
+        try:
+            self.client.close()
+        except Exception as e:
+            print(f"[ ERROR ]: Failed to close session.")
 
-    def list_databases():
-        pass
+    def list_databases(self):
+        try:
+            databases = self.client.query_df(f"SHOW DATABASES")
+            return databases
+        except Exception as e:
+            print(f"[ ERROR ] : Failed to list DB's")
+            return None
 
     def create_database():
         pass
 
     def delete_database():
         pass
+
+test = ClickhouseConnector()
+test.close_session()
