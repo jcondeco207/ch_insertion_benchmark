63aff71e19bc65a8d20d42b9b91e7896b6ea0dea
jcondeco207 <joao.r.condeco@gmail.com>
2025-07-09 19:25:23 +0100

    Update Docker Compose configuration and Go files for ClickHouse connection

--- go_benchmarks/clickhouse_go_bm/cmd/bm1/main.go
+++ go_benchmarks/clickhouse_go_bm/cmd/bm1/main.go
@@ -1,1 +1,17 @@
 package main
+
+import (
+	"fmt"
+
+	"jcondeco.com/bm1/internal/clickhouse"
+)
+
+func main() {
+	fmt.Println("Hello there")
+	_, err := clickhouse.Connect()
+	if err != nil {
+		fmt.Println("Failed to connect:", err)
+		return
+	}
+
+}
--- go_benchmarks/clickhouse_go_bm/internal/clickhouse/clickhouse.go
+++ go_benchmarks/clickhouse_go_bm/internal/clickhouse/clickhouse.go
@@ -1,1 +1,50 @@
 package clickhouse
+
+import (
+	"context"
+	"crypto/tls"
+	"fmt"
+
+	"github.com/ClickHouse/clickhouse-go/v2"
+	"github.com/ClickHouse/clickhouse-go/v2/lib/driver"
+)
+
+func Connect() (driver.Conn, error) {
+	var (
+		ctx       = context.Background()
+		conn, err = clickhouse.Open(&clickhouse.Options{
+			Addr: []string{"127.0.0.1:9440"},
+			Auth: clickhouse.Auth{
+				Database: "default",
+				Username: "demo",
+				Password: "demo",
+			},
+			ClientInfo: clickhouse.ClientInfo{
+				Products: []struct {
+					Name    string
+					Version string
+				}{
+					{Name: "an-example-go-client", Version: "0.1"},
+				},
+			},
+			Debugf: func(format string, v ...interface{}) {
+				fmt.Printf(format, v)
+			},
+			TLS: &tls.Config{
+				InsecureSkipVerify: true,
+			},
+		})
+	)
+
+	if err != nil {
+		return nil, err
+	}
+
+	if err := conn.Ping(ctx); err != nil {
+		if exception, ok := err.(*clickhouse.Exception); ok {
+			fmt.Printf("Exception [%d] %s \n%s\n", exception.Code, exception.Message, exception.StackTrace)
+		}
+		return nil, err
+	}
+	return conn, nil
+}
